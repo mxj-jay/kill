84f071794c90d9a36d9347252c7bbd0ae566741e
mxj-jay <597428285@qq.com>
2019-12-27 22:28:48 +0800

    Html邮件提醒内容发送

--- server/src/main/java/com/debug/kill/server/service/MailService.java
+++ server/src/main/java/com/debug/kill/server/service/MailService.java
@@ -3,9 +3,17 @@ package com.debug.kill.server.service;
 import com.debug.kill.server.dto.MailDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author Damocles
@@ -16,13 +24,44 @@ public class MailService {
 
     private static final Logger logger = LoggerFactory.getLogger(MailService.class);
 
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+
     @Async
     public void sendSimpleMail(MailDto mailDto){
         try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom(env.getProperty("mail.send.from"));
+            simpleMailMessage.setTo(mailDto.getTos());
+            simpleMailMessage.setSubject(mailDto.getSubject());
+            simpleMailMessage.setText(mailDto.getContent());
+            javaMailSender.send(simpleMailMessage);
 
             logger.info("发送简单文本--发送成功!");
         }catch (Exception e){
             logger.error("发送简单文本文件--发送异常:",e.fillInStackTrace());
+        }
+    }
+
+    @Async
+    public void sendHtmlMail(MailDto mailDto){
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true,"utf-8");
+            mimeMessageHelper.setFrom(env.getProperty("mail.send.from"));
+            mimeMessageHelper.setTo(mailDto.getTos());
+            mimeMessageHelper.setSubject(mailDto.getSubject());
+            mimeMessageHelper.setText(mailDto.getContent(),true);
+            javaMailSender.send(mimeMessage);
+
+            logger.info("发送Html文本--发送成功!");
+        }catch (Exception e){
+            logger.error("发送Html文本文件--发送异常:",e.fillInStackTrace());
         }
     }
 
--- server/src/main/java/com/debug/kill/server/service/RabbitReceiverService.java
+++ server/src/main/java/com/debug/kill/server/service/RabbitReceiverService.java
@@ -4,6 +4,7 @@ import com.debug.kill.model.dto.KillSuccessUserInfo;
 import com.debug.kill.model.entity.ItemKillSuccess;
 import com.debug.kill.model.mapper.ItemKillSuccessMapper;
 import com.debug.kill.server.dto.MailDto;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -21,19 +22,30 @@ public class RabbitReceiverService {
 
     public static final Logger logger= LoggerFactory.getLogger(RabbitReceiverService.class);
 
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private MailService mailService;
+
     @RabbitListener(queues = {"${mq.kill.item.success.email.queue}"}, containerFactory = "singleListenerContainer")
     public void consumeEmailMsg(KillSuccessUserInfo info) {
         try {
             logger.info("秒杀异步邮件通知-秒杀信息:{}", info);
 
-            //TODO: 真正的发送邮件逻辑
-
-
+            //TODO: 真正的发送邮件逻辑 -- 封装一个MailDto进行数据传输
+//            MailDto mailDto = new MailDto(env.getProperty("mail.kill.item.success.subject"),"这是一个简单文本~",new String[]{info.getEmail()});
+//            mailService.sendSimpleMail(mailDto);
+            String content = String.format(env.getProperty("mail.kill.item.success.content"),info.getItemName(),info.getCode());
+            MailDto mailDto = new MailDto(env.getProperty("mail.kill.item.success.subject"),content,new String[]{info.getEmail()});
+            mailService.sendHtmlMail(mailDto);
 
         }catch (Exception e){
             logger.error("秒杀异步邮件通知-接收消息-发生异常：",e.fillInStackTrace());
         }
     }
+
+
 
 
 }
